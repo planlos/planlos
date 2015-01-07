50d1e023cc56f6d21a88c46fc84d18783ff8f68e
Planlos #2 <termine@planlos-bremen.de>
2015-01-07 22:36:13 +0100

    some imporvements to event

--- app/angular/static/app/scripts/controllers/event.js
+++ app/angular/static/app/scripts/controllers/event.js
@@ -8,8 +8,11 @@
  * Controller of the staticApp
  */
 angular.module('staticApp')
-  .controller('EventCtrl', function ($http, $routeParams, $scope) {
+  .controller('EventCtrl', function ($q, $http, $routeParams, $scope) {
   	var eventId = $routeParams['id'];
+
+  	var EVENT = 'event';
+  	var LOCATIONS = 'locations';
 
     $scope.dateOptions = {
         yearRange: '2015:-0',
@@ -18,15 +21,40 @@ angular.module('staticApp')
 
   	if(eventId){
   		//update event
-  		var respPromise = $http.get('http://localhost:5000/api/events/' + eventId);
-  		respPromise.success((function(data, status, headers, config){
-		$scope.event = data;
-			console.log(data);
-		}));
+  		var eventsPromise = $http.get('http://192.168.1.41:5000/api/events/' + eventId, {key: EVENT});
+  		var locationsPromise = $http.get('http://192.168.1.41:5000/api/locations/', {key: LOCATIONS});
+
+  		/*$q.all([eventsPromise, locationsPromise]).then(function(result){
+  			var tmp = [];
+  			console.log(result);
+  			angular.forEach(result, function(response){
+
+  			});
+  		})*/
+  		eventsPromise.success(
+        function(data, status, headers, config){
+		      $scope.event = data;
+		    }
+    	);
+		locationsPromise.success(
+        function(data, status, headers, config){
+		      $scope.locations = data.locations;
+		    }
+    	);
   	}
   	else{
 	  	$scope.event = {};
+	  	var locationsPromise = $http.get('http://192.168.1.41:5000/api/locations/');
 	  	//create new event
+  	}
+
+  	$scope.saveEvent = function(){
+  		var eventsPromise = $http.post('http://192.168.1.41:5000/api/events/' + $scope.event.id, $scope.event);
+  		eventsPromise.success(
+  			function(data, status, headers, config){
+		      console.log(data);
+		    }
+  		);
   	}
 
   });
